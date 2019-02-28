ee3b081602c953313d542b406e093d9167c7fb89
Gilberto Lopez Lopez <gilbertoll@muni-carta.go.cr>
2019-02-28 17:18:36 -0600

    Tarea 1 completada

---
+++ advinanza/advinanza/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace advinanza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+          
+            InitializeComponent();
+            BotonSorteo.Text = "Click aquí, para validar su número";
+            BotonSorteo.Clicked += BotonSorteo_Clicked;
+            Numero.Text = "Digite el número para adivinar";
+            
+
+            
+        }
+
+        private void BotonSorteo_Clicked(object sender, EventArgs e)
+        {
+            var adivinanza = "500";
+            Console.WriteLine ("Digite el número: ");
+            Numero.Text = "Digite el numero";
+            if (adivinanza == Entrada.Text)
+                Numero.Text = "Exito";
+            else
+                Numero.Text = "Numero incorrecto";
+
+          
+            
+        }
+    }
+}
